524535bb9fbb5b494e1df9895d11aec595db365a
monolab825 <p.smiles777@gmail.com>
2024-04-26 05:01:39 -0500

    added frontend ui to toggle components to render

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,44 +1,71 @@
 "use client";
 
+import { useState } from "react";
 import type { NextPage } from "next";
 import { NftCard } from "~~/components/nft-card/NftCard";
 // import { NameCard, NameCardProps } from "~~/components/NftCardValue/NameCard";
-import { useToken } from "~~/hooks/useToken";
+import { useToken, useTokens } from "~~/hooks/useToken";
+
+const test: any[] = [
+  "Image",
+  "Name",
+  "Description",
+  "Attributes",
+  "Address",
+  "CollectionName",
+  "CollectionSymbol",
+  "Id",
+];
 
 const Home: NextPage = () => {
+  const [checkeds, setCheckeds] = useState<boolean[]>([true, true, true, true, true, true, true, true]);
+
+  const handleChange = (index: number) => {
+    const temp: boolean[] = checkeds;
+
+    temp[index] = !temp[index];
+
+    setCheckeds([...temp]);
+  };
+
+  const inputComponents = test.map((inputName, index) => {
+    return (
+      <div key={index + "-input"}>
+        <label className="m-1">
+          {inputName}
+          <input
+            className="m-1"
+            type="checkbox"
+            checked={checkeds[index]}
+            onChange={() => {
+              handleChange(index);
+            }}
+          />
+        </label>
+      </div>
+    );
+  });
+
   const token = useToken(BigInt(4), "nftstorage");
 
-  //414x264
+  const tokens = useTokens([BigInt(1), BigInt(2), BigInt(3), BigInt(4)], "nftstorage");
+
+  const componentsToRender: any = [];
+  for (let i = 0; i < checkeds.length; i++) {
+    if (checkeds[i]) componentsToRender.push(test[i]);
+  }
+
+  const tokensComponents = tokens.map((token, index) => {
+    return <NftCard key={index} token={token} renderOrder={componentsToRender} />;
+  });
+
   return (
     <div>
-      {/* <div className="flex justify-center">
-        <p>hello</p>
-      </div> */}
-
+      <div className="flex flex-wrap">{inputComponents}</div>
       <div className="flex flex-wrap justify-center m-1 p-1 bg-base-100 rounded">
-        <NftCard token={token} />
-        <NftCard token={token} />
-        <NftCard token={token} />
-        <NftCard token={token} />
-        <NftCard token={token} />
-        <NftCard token={token} />
+        {tokensComponents}
         <NftCard token={token} />
       </div>
-
-      {/* <div className="flex justify-center">
-        <NftCard token={token} />
-      </div> */}
-
-      {/* <button
-        onClick={async () => {
-          await writeErc721Async({
-            functionName: "mint",
-            args: [connectedAddress],
-          });
-        }}
-      >
-        Mint
-      </button> */}
     </div>
   );
 };
--- packages/nextjs/hooks/useToken.tsx
+++ packages/nextjs/hooks/useToken.tsx
@@ -1,11 +1,105 @@
 "use client";
 
+import { useCallback, useEffect, useState } from "react";
 import { useFetch } from "usehooks-ts";
 import { useScaffoldContract, useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 const replacement = {
   ipfs: "https://ipfs.io/ipfs/",
   nftstorage: "https://nftstorage.link/ipfs/",
+};
+
+function useFetches(uris: string[]) {
+  const [responses, setResponses] = useState<any[]>([]);
+
+  const refetch = useCallback(async () => {
+    const arr = [];
+    for (let i = 0; i < uris.length; i++) {
+      const response = await fetch(uris[i]);
+      const responseJson = await response.json();
+      arr.push(responseJson);
+    }
+
+    setResponses([...arr]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [uris.length]);
+
+  useEffect(() => {
+    async function get() {
+      await refetch();
+    }
+
+    get();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [uris.length, refetch]);
+
+  return { responses, refetch };
+}
+
+export function useUris(contract: any, tokenIds: bigint[]) {
+  const [uris, setUris] = useState<string[]>([]);
+
+  const refetch = useCallback(async () => {
+    if (!contract) return;
+
+    const arr = [];
+    for (let i = 0; i < tokenIds.length; i++) {
+      const result = await contract.read.tokenURI([tokenIds[i]]);
+      arr.push(result);
+    }
+
+    setUris([...arr]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [contract?.address, tokenIds.length, uris.length]);
+
+  useEffect(() => {
+    async function get() {
+      await refetch();
+    }
+
+    get();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [contract?.address, tokenIds.length, uris.length, refetch]);
+
+  return { uris, setUris, refetch };
+}
+
+export const useTokens = (tokenIds: bigint[], replacementType: "ipfs" | "nftstorage" = "ipfs") => {
+  const { data: scaffoldErc721 } = useScaffoldContract({ contractName: "ScaffoldERC721" });
+
+  const { data: collectionName } = useScaffoldReadContract({
+    contractName: "ScaffoldERC721",
+    functionName: "name",
+  });
+
+  const { data: collectionSymbol } = useScaffoldReadContract({
+    contractName: "ScaffoldERC721",
+    functionName: "symbol",
+  });
+
+  const { uris } = useUris(scaffoldErc721, tokenIds);
+
+  for (let i = 0; i < uris.length; i++) {
+    uris[i] = uris[i].replace("ipfs://", replacement[replacementType]);
+  }
+
+  const { responses } = useFetches(uris);
+
+  const tokens: any[] = [];
+  for (let i = 0; i < responses.length; i++) {
+    responses[i] ? (responses[i].image = responses[i].image.replace("ipfs://", replacement[replacementType])) : <></>;
+
+    const token = {} as any;
+    token.contract = scaffoldErc721;
+    token.metadata = responses[i];
+    token.id = tokenIds[i];
+    token.uri = uris[i];
+    token.collectionName = collectionName;
+    token.collectionSymbol = collectionSymbol;
+    tokens.push(token);
+  }
+
+  return tokens;
 };
 
 export const useToken = (tokenId: bigint, replacementType: "ipfs" | "nftstorage" = "ipfs") => {
@@ -16,8 +110,6 @@ export const useToken = (tokenId: bigint, replacementType: "ipfs" | "nftstorage"
     functionName: "tokenURI",
     args: [tokenId],
   });
-
-  console.log(tokenURI);
 
   const { data: collectionName } = useScaffoldReadContract({
     contractName: "ScaffoldERC721",
