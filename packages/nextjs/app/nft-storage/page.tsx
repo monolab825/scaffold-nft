b361a29d5f701917303cbf4542d35627c4d6b4d0
monolab825 <p.smiles777@gmail.com>
2024-04-29 18:42:13 -0500

    latest

--- packages/nextjs/app/nft-storage/page.tsx
+++ packages/nextjs/app/nft-storage/page.tsx
@@ -1,34 +1,63 @@
 "use client";
 
+import { create } from "@web3-storage/w3up-client";
 import type { NextPage } from "next";
-import { NFTStorage } from "nft.storage";
 
 const NftStoragePage: NextPage = () => {
   const onSubmit = async (event: any) => {
     event.preventDefault();
 
-    console.log(event);
+    // let res = await fetch("http://localhost:8080", {
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json",
+    //   },
+    //   method: "POST",
+    //   body: JSON.stringify({ a: 1, b: "Textual content" }),
+    // });
 
-    const client = new NFTStorage({ token: "abfdf65a.cb58d9d119bb41bf84445fc53f235f1e" });
+    // console.log(await res.json());
 
-    const nameInput = event.target["0"] as any;
-    const fileInput = event.target["1"] as any;
-    console.log(nameInput.value);
-    console.log(fileInput.value);
+    const client = await create();
 
     try {
-      const metadata = await client.store({
-        name: "This is a test name",
-        description: "Using the nft.storage metadata API to create ERC-1155 compatible metadata.",
-        image: fileInput.files[0],
-      });
+      console.log("Logging in...");
 
-      console.log({ "IPFS URL for the metadata": metadata.url });
-      console.log({ "metadata.json contents": metadata.data });
-      console.log({ "metadata.json contents with IPFS gateway URLs": metadata.embed() });
-    } catch (err) {
-      console.error(err);
+      await client.login("homanicsjake@gmail.com");
+      //setPleaseCheckYourEmail(true);
+      console.log("Logged in!");
+    } catch (e) {
+      console.log(e);
+      return e;
     }
+
+    // try {
+    //   await client.setCurrentSpace("did:key:z6MkhCcXJFhNaNTcjQ1ZVRdccBH61PBuUb1AZ1xK4GgyeGPe");
+    //   console.log("Current space set!");
+    // } catch (e) {
+    //   console.log(e);
+    // }
+
+    // const client = new NFTStorage({ token: "abfdf65a.cb58d9d119bb41bf84445fc53f235f1e" });
+
+    // const nameInput = event.target["0"] as any;
+    // const fileInput = event.target["1"] as any;
+    // console.log(nameInput.value);
+    // console.log(fileInput.value);
+
+    // try {
+    //   const metadata = await client.store({
+    //     name: "This is a test name",
+    //     description: "Using the nft.storage metadata API to create ERC-1155 compatible metadata.",
+    //     image: fileInput.files[0],
+    //   });
+
+    //   console.log({ "IPFS URL for the metadata": metadata.url });
+    //   console.log({ "metadata.json contents": metadata.data });
+    //   console.log({ "metadata.json contents with IPFS gateway URLs": metadata.embed() });
+    // } catch (err) {
+    //   console.error(err);
+    // }
   };
 
   return (
--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   31337: {
     Constraints: {
-      address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
       abi: [
         {
           type: "function",
@@ -42,7 +42,7 @@ const deployedContracts = {
       inheritedFunctions: {},
     },
     ScaffoldERC721: {
-      address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+      address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
       abi: [
         {
           type: "constructor",
@@ -599,7 +599,20 @@ const deployedContracts = {
           inputs: [],
         },
       ],
-      inheritedFunctions: {},
+      inheritedFunctions: {
+        approve: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+        balanceOf: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+        getApproved: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+        isApprovedForAll: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+        name: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+        ownerOf: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+        safeTransferFrom: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+        setApprovalForAll: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+        supportsInterface: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+        symbol: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+        tokenURI: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+        transferFrom: "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol",
+      },
     },
   },
 } as const;
