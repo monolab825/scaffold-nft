63b4839cb4ccdffef0ab0373377485b23255a3ae
monolab825 <p.smiles777@gmail.com>
2024-05-01 01:43:20 -0500

    set up testing grounds

--- packages/nextjs/app/testing-grounds/page.tsx
+++ packages/nextjs/app/testing-grounds/page.tsx
@@ -3,6 +3,10 @@
 import { useState } from "react";
 import type { NextPage } from "next";
 import { NftCard } from "~~/components/nft-card/NftCard";
+import { AddressCard, AddressCardProps } from "~~/components/nft-card/values/AddressCard";
+import { CollectionNameCard, CollectionNameCardProps } from "~~/components/nft-card/values/CollectionNameCard";
+import { CollectionSymbolCard, CollectionSymbolCardProps } from "~~/components/nft-card/values/CollectionSymbolCard";
+import { CollectionDetails } from "~~/components/nft-card/values/extensions/CollectionDetails";
 // import { NameCard, NameCardProps } from "~~/components/NftCardValue/NameCard";
 import { useTokens } from "~~/hooks/useToken";
 
@@ -16,6 +20,18 @@ const inputOptions: any[] = [
   "CollectionName",
   "CollectionSymbol",
 ];
+
+const AddressCardComponent = (props: AddressCardProps) => {
+  return <AddressCard {...props} bgColor="bg-base-300" />;
+};
+
+const CollectionNameCardComponent = (props: CollectionNameCardProps) => {
+  return <CollectionNameCard {...props} bgColor="bg-base-300" />;
+};
+
+const CollectionSymbolCardComponent = (props: CollectionSymbolCardProps) => {
+  return <CollectionSymbolCard {...props} bgColor="bg-base-300" />;
+};
 
 const TestingGrounds: NextPage = () => {
   const [checkeds, setCheckeds] = useState<boolean[]>([true, true, true, true, true, true, true, true]);
@@ -54,7 +70,7 @@ const TestingGrounds: NextPage = () => {
   // const token = useToken(BigInt(1), "w3s");
 
   const arr = [];
-  for (let i = 1; i <= 4; i++) {
+  for (let i = 1; i <= 10; i++) {
     arr.push(BigInt(i));
   }
   const tokens = useTokens(arr, "w3s");
@@ -66,6 +82,17 @@ const TestingGrounds: NextPage = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-wrap">{inputComponents}</div>
+      <div className="w-full">
+        <CollectionDetails
+          token={tokens[0]}
+          showDescriptor={true}
+          bgColor="bg-base-100"
+          AddressCard={AddressCardComponent}
+          CollectionNameCard={CollectionNameCardComponent}
+          CollectionSymbolCard={CollectionSymbolCardComponent}
+        />
+      </div>
+
       <div className="flex flex-wrap justify-center m-1 p-1 bg-base-100 rounded lg:max-w-[1300px]">
         {tokensComponents}
         {/* <NftCard token={token} /> */}
