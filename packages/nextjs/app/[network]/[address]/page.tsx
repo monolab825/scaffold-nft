997bece6c6037857ff449be3ebfd6b7297dd83c5
monolab825 <p.smiles777@gmail.com>
2024-04-30 21:50:32 -0500

    Update page.tsx

--- packages/nextjs/app/[network]/[address]/page.tsx
+++ packages/nextjs/app/[network]/[address]/page.tsx
@@ -145,51 +145,72 @@ export default function Collection({ params }: { params: { network: string; addr
     // setRenderedTokenIds([...tempArr]);
   }
 
+  const [isShowingAdvancedSettings, setIsShowingAdvancedSettings] = useState(false);
+
+  let advancedOutput;
+  if (isShowingAdvancedSettings) {
+    advancedOutput = (
+      <>
+        <div className="flex flex-wrap">{inputComponents}</div>
+        <form
+          onSubmit={(event: any) => {
+            onSubmit(event);
+          }}
+        >
+          <div className="bg-base-100 rounded p-1 flex flex-col items-center justify-center ">
+            <p className="text-center m-0">Number To Render</p>
+
+            <div className={`flex border-2 border-base-300 bg-base-200 rounded-full text-accent`}>
+              <input
+                className="input input-ghost focus-within:border-transparent focus:outline-none focus:bg-transparent focus:text-gray-400 h-[2.2rem] min-h-[2.2rem] px-4 border w-full font-medium placeholder:text-accent/50 text-gray-400"
+                placeholder={"1"}
+                defaultValue={5}
+                // onChange={(event: any) => {
+                //   setNumToRender(event.target.value);
+                // }}
+              />
+              <br />
+            </div>
+
+            <p className="text-center m-0">Start Index</p>
+
+            <div className={`flex border-2 border-base-300 bg-base-200 rounded-full text-accent`}>
+              <input
+                className="input input-ghost focus-within:border-transparent focus:outline-none focus:bg-transparent focus:text-gray-400 h-[2.2rem] min-h-[2.2rem] px-4 border w-full font-medium placeholder:text-accent/50 text-gray-400"
+                placeholder={"1"}
+                defaultValue={1}
+                // onChange={(event: any) => {
+                //   setStartIndex(event.target.value);
+                // }}
+              />
+            </div>
+            <p className="text-center m-0">Metadata Load Type</p>
+            <div className="w-64">
+              <Select options={options} className="text-black bg-base-100" onChange={onChange2} values={options} />
+            </div>
+
+            <button type="submit" className="btn btn-sm btn-primary">
+              Refresh
+            </button>
+          </div>
+        </form>
+      </>
+    );
+  } else {
+    advancedOutput = <></>;
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="flex flex-wrap">{inputComponents}</div>
-      <form
-        onSubmit={(event: any) => {
-          onSubmit(event);
+      <button
+        onClick={() => {
+          setIsShowingAdvancedSettings(!isShowingAdvancedSettings);
         }}
+        className="btn btn-sm btn-primary"
       >
-        <div className="bg-base-100 rounded p-1 flex flex-col items-center justify-center ">
-          <p className="text-center m-0">Number To Render</p>
-
-          <div className={`flex border-2 border-base-300 bg-base-200 rounded-full text-accent`}>
-            <input
-              className="input input-ghost focus-within:border-transparent focus:outline-none focus:bg-transparent focus:text-gray-400 h-[2.2rem] min-h-[2.2rem] px-4 border w-full font-medium placeholder:text-accent/50 text-gray-400"
-              placeholder={"1"}
-              defaultValue={5}
-              // onChange={(event: any) => {
-              //   setNumToRender(event.target.value);
-              // }}
-            />
-            <br />
-          </div>
-
-          <p className="text-center m-0">Start Index</p>
-
-          <div className={`flex border-2 border-base-300 bg-base-200 rounded-full text-accent`}>
-            <input
-              className="input input-ghost focus-within:border-transparent focus:outline-none focus:bg-transparent focus:text-gray-400 h-[2.2rem] min-h-[2.2rem] px-4 border w-full font-medium placeholder:text-accent/50 text-gray-400"
-              placeholder={"1"}
-              defaultValue={1}
-              // onChange={(event: any) => {
-              //   setStartIndex(event.target.value);
-              // }}
-            />
-          </div>
-          <p className="text-center m-0">Metadata Load Type</p>
-          <div className="w-64">
-            <Select options={options} className="text-black bg-base-100" onChange={onChange2} values={options} />
-          </div>
-
-          <button type="submit" className="btn btn-sm btn-primary">
-            Refresh
-          </button>
-        </div>
-      </form>
+        Toggled Advanced Options
+      </button>
+      {advancedOutput}
       <div className="w-full">
         <CollectionDetails
           token={tokens[0]}
