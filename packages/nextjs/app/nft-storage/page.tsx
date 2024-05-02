d456673fff0a33fd72ce4f2db83ee6a8bca5e522
monolab825 <p.smiles777@gmail.com>
2024-05-02 01:25:17 -0500

    Update page.tsx

--- packages/nextjs/app/nft-storage/page.tsx
+++ packages/nextjs/app/nft-storage/page.tsx
@@ -9,9 +9,9 @@
 //   Provider,
 // } from "@w3ui/react";
 import { useEffect } from "react";
-// import * as DID from "@ipld/dag-ucan/did";
-// import { connect } from "@ucanto/client";
-// import { CAR, HTTP } from "@ucanto/transport";
+import * as DID from "@ipld/dag-ucan/did";
+import { connect } from "@ucanto/client";
+import { CAR, HTTP } from "@ucanto/transport";
 import { StoreIndexedDB } from "@web3-storage/access/stores/store-indexeddb";
 // import scaffoldConfig from "~~/scaffold.config";
 // import { create } from "@web3-storage/w3up-client";
@@ -22,25 +22,25 @@ import type { NextPage } from "next";
 const DB_NAME = "@w3ui";
 const DB_STORE_NAME = "core";
 
-// function createServiceConf({ servicePrincipal, connection }: any = {}) {
-//   const id = servicePrincipal != null ? servicePrincipal : DID.parse("did:web:web3.storage");
-//   const serviceConnection =
-//     connection != null
-//       ? connection
-//       : connect({
-//           id,
-//           codec: CAR.outbound,
-//           channel: HTTP.open<any>({
-//             url: new URL("https://up.web3.storage"),
-//             method: "POST",
-//           }),
-//         });
-//   return {
-//     access: serviceConnection,
-//     upload: serviceConnection,
-//     filecoin: serviceConnection,
-//   };
-// }
+function createServiceConf({ servicePrincipal, connection }: any = {}) {
+  const id = servicePrincipal != null ? servicePrincipal : DID.parse("did:web:web3.storage");
+  const serviceConnection =
+    connection != null
+      ? connection
+      : connect({
+          id,
+          codec: CAR.outbound,
+          channel: HTTP.open<any>({
+            url: new URL("https://up.web3.storage"),
+            method: "POST",
+          }),
+        });
+  return {
+    access: serviceConnection,
+    upload: serviceConnection,
+    filecoin: serviceConnection,
+  };
+}
 
 /**
  * An IndexedDB store that dispatches an event on the passed EventTarget when
@@ -71,7 +71,8 @@ const DB_STORE_NAME = "core";
   const dbName = `${DB_NAME}${options?.servicePrincipal != null ? "@" + options?.servicePrincipal.did() : ""}`;
   const events = options?.events ?? new EventTarget();
   const store = new IndexedDBEventDispatcherStore(dbName, events);
-  // const serviceConf = createServiceConf(options);
+  //const serviceConf =
+  createServiceConf(options);
   // const client = await create({
   //   store,
   //   serviceConf,
