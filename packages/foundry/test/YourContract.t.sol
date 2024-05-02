6c7727bc4a995ade51762bd6ce9fbe67daa58fce
monolab825 <p.smiles777@gmail.com>
2024-05-01 22:03:34 -0500

    added foundry project

--- packages/foundry/test/YourContract.t.sol
+++ packages/foundry/test/YourContract.t.sol
@@ -1,28 +1,28 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../contracts/YourContract.sol";
+// import "forge-std/Test.sol";
+// import "../contracts/YourContract.sol";
 
-contract YourContractTest is Test {
-    YourContract public yourContract;
+// contract YourContractTest is Test {
+//     YourContract public yourContract;
 
-    function setUp() public {
-        yourContract = new YourContract(vm.addr(1));
-    }
+//     function setUp() public {
+//         yourContract = new YourContract(vm.addr(1));
+//     }
 
-    function testMessageOnDeployment() public view {
-        require(
-            keccak256(bytes(yourContract.greeting()))
-                == keccak256("Building Unstoppable Apps!!!")
-        );
-    }
+//     function testMessageOnDeployment() public view {
+//         require(
+//             keccak256(bytes(yourContract.greeting()))
+//                 == keccak256("Building Unstoppable Apps!!!")
+//         );
+//     }
 
-    function testSetNewMessage() public {
-        yourContract.setGreeting("Learn Scaffold-ETH 2! :)");
-        require(
-            keccak256(bytes(yourContract.greeting()))
-                == keccak256("Learn Scaffold-ETH 2! :)")
-        );
-    }
-}
+//     function testSetNewMessage() public {
+//         yourContract.setGreeting("Learn Scaffold-ETH 2! :)");
+//         require(
+//             keccak256(bytes(yourContract.greeting()))
+//                 == keccak256("Learn Scaffold-ETH 2! :)")
+//         );
+//     }
+// }
