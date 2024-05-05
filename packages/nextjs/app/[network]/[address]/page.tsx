09155216fcf7fef6accdc86b1bffc1e9b573443b
monolab825 <p.smiles777@gmail.com>
2024-05-04 19:47:21 -0500

    removed raw token functions from dynamic page

--- packages/nextjs/app/[network]/[address]/page.tsx
+++ packages/nextjs/app/[network]/[address]/page.tsx
@@ -1,30 +1,13 @@
 "use client";
 
 import React from "react";
-// import { useState } from "react";
 import "react-dropdown/style.css";
 import { renderInputOptions } from "~~/app/nftCollectionPagesConfig";
-import { NftCard } from "~~/components/nft-card/NftCard";
-import { AddressCard, AddressCardProps } from "~~/components/nft-card/values/AddressCard";
-import { CollectionNameCard, CollectionNameCardProps } from "~~/components/nft-card/values/CollectionNameCard";
-import { CollectionSymbolCard, CollectionSymbolCardProps } from "~~/components/nft-card/values/CollectionSymbolCard";
-import { CollectionDetails } from "~~/components/nft-card/values/extensions/CollectionDetails";
+import { Tokens } from "~~/components/Tokens";
 import useAdvancedFiltering from "~~/hooks/useAdvancedFiltering";
 import useCheckboxes from "~~/hooks/useCheckboxes";
 import useTokenIds from "~~/hooks/useTokenIds";
 import { useTokens } from "~~/hooks/useTokens2";
-
-const AddressCardComponent = (props: AddressCardProps) => {
-  return <AddressCard {...props} bgColor="bg-base-300" />;
-};
-
-const CollectionNameCardComponent = (props: CollectionNameCardProps) => {
-  return <CollectionNameCard {...props} bgColor="bg-base-300" />;
-};
-
-const CollectionSymbolCardComponent = (props: CollectionSymbolCardProps) => {
-  return <CollectionSymbolCard {...props} bgColor="bg-base-300" />;
-};
 
 export default function Collection({ params }: { params: { network: string; address: string } }) {
   const { inputComponents, componentsToRender } = useCheckboxes(renderInputOptions);
@@ -38,34 +21,10 @@ export default function Collection({ params }: { params: { network: string; addr
 
   const { tokens, isLoading, isError } = useTokens(params["network"], params["address"], tokenIds, chosenOption);
 
-  const tokensComponents = tokens.map((token, index) => {
-    return <NftCard key={index} token={token} renderOrder={componentsToRender} />;
-  });
-
-  let mainContent;
-  if (isLoading) {
-    mainContent = <p>Loading...</p>;
-  } else {
-    if (isError) {
-      mainContent = <p>There was an error. Please try changing the advanced settings.</p>;
-    } else {
-      mainContent = tokensComponents;
-    }
-  }
   return (
     <div className="flex flex-col items-center justify-center">
       {advancedOutput}
-      <div className="w-full">
-        <CollectionDetails
-          token={tokens[0]}
-          showDescriptor={true}
-          bgColor="bg-base-100"
-          AddressCard={AddressCardComponent}
-          CollectionNameCard={CollectionNameCardComponent}
-          CollectionSymbolCard={CollectionSymbolCardComponent}
-        />
-      </div>
-      <div className="flex flex-wrap justify-center m-1 p-1 bg-base-100 rounded lg:max-w-[1300px]">{mainContent}</div>{" "}
+      <Tokens tokens={tokens} isLoading={isLoading} isError={isError} renderOrder={componentsToRender} />
     </div>
   );
 }
--- packages/nextjs/app/testing-grounds/page.tsx
+++ packages/nextjs/app/testing-grounds/page.tsx
@@ -1,30 +1,12 @@
 "use client";
 
-// import { useState } from "react";
 import { renderInputOptions } from "../nftCollectionPagesConfig";
 import type { NextPage } from "next";
 import { Tokens } from "~~/components/Tokens";
-// import { NftCard } from "~~/components/nft-card/NftCard";
-// import { AddressCard, AddressCardProps } from "~~/components/nft-card/values/AddressCard";
-// import { CollectionNameCard, CollectionNameCardProps } from "~~/components/nft-card/values/CollectionNameCard";
-// import { CollectionSymbolCard, CollectionSymbolCardProps } from "~~/components/nft-card/values/CollectionSymbolCard";
-// import { CollectionDetails } from "~~/components/nft-card/values/extensions/CollectionDetails";
 import useAdvancedFiltering from "~~/hooks/useAdvancedFiltering";
 import useCheckboxes from "~~/hooks/useCheckboxes";
 import { useTokens } from "~~/hooks/useToken";
 import useTokenIds from "~~/hooks/useTokenIds";
-
-// const AddressCardComponent = (props: AddressCardProps) => {
-//   return <AddressCard {...props} bgColor="bg-base-300" />;
-// };
-
-// const CollectionNameCardComponent = (props: CollectionNameCardProps) => {
-//   return <CollectionNameCard {...props} bgColor="bg-base-300" />;
-// };
-
-// const CollectionSymbolCardComponent = (props: CollectionSymbolCardProps) => {
-//   return <CollectionSymbolCard {...props} bgColor="bg-base-300" />;
-// };
 
 const TestingGrounds: NextPage = () => {
   const { inputComponents, componentsToRender } = useCheckboxes(renderInputOptions);
@@ -38,36 +20,10 @@ const TestingGrounds: NextPage = () => {
 
   const { tokens, isLoading, isError } = useTokens(tokenIds, chosenOption);
 
-  // const tokensComponents = tokens.map((token, index) => {
-  //   return <NftCard key={index} token={token} renderOrder={componentsToRender} />;
-  // });
-
-  // let mainContent;
-  // if (isLoading) {
-  //   mainContent = <p>Loading...</p>;
-  // } else {
-  //   if (isError) {
-  //     mainContent = <p>There was an error. Please try changing the advanced settings.</p>;
-  //   } else {
-  //     mainContent = tokensComponents;
-  //   }
-  // }
-
   return (
     <div className="flex flex-col items-center justify-center">
       {advancedOutput}
       <Tokens tokens={tokens} isLoading={isLoading} isError={isError} renderOrder={componentsToRender} />
-      {/* <div className="w-full">
-        <CollectionDetails
-          token={tokens[0]}
-          showDescriptor={true}
-          bgColor="bg-base-100"
-          AddressCard={AddressCardComponent}
-          CollectionNameCard={CollectionNameCardComponent}
-          CollectionSymbolCard={CollectionSymbolCardComponent}
-        />
-      </div>
-      <div className="flex flex-wrap justify-center m-1 p-1 bg-base-100 rounded lg:max-w-[1300px]">{mainContent}</div>{" "} */}
     </div>
   );
 };
