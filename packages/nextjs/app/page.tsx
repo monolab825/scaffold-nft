4e6d0ddddf92961a7f14d26471de92073a8c0268
monolab825 <p.smiles777@gmail.com>
2024-05-06 20:46:49 -0500

    added search page on homepage

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,15 +1,41 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
+import Select from "react-dropdown-select";
+import * as allChains from "viem/chains";
 import {
   BeakerIcon, // CogIcon, // BugAntIcon, MagnifyingGlassIcon
 } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
+  const options = [];
+
+  const keys = Object.keys(allChains);
+
+  for (const key of keys) {
+    options.push({
+      value: key,
+      label: key,
+    });
+  }
+
+  const defaultOptionIndex = options
+    .map(function (e) {
+      return e.value;
+    })
+    .indexOf("mainnet");
+
+  const defaultOption = options[defaultOptionIndex];
+
+  const [selectedDropdownOption, setSelectedDropdownOption] = useState(defaultOption);
+
+  async function onSubmit(e: any) {
+    e.preventDefault();
+
+    window.location.href = "https://ladders.vision/" + selectedDropdownOption + "/" + e.target[0].value;
+  }
 
   return (
     <>
@@ -19,26 +45,36 @@ const Home: NextPage = () => {
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">Scaffold-NFT</span>
           </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+
+          <form onSubmit={onSubmit} className="flex flex-col space-y-4">
+            <div>
+              <p>Select the blockchain that your NFT collection exists on.</p>
+              <Select
+                options={options}
+                className="text-black bg-base-100"
+                onChange={(event: any) => {
+                  if (event[0]) setSelectedDropdownOption(event[0].value);
+                }}
+                values={[defaultOption]}
+              />
+            </div>
+
+            <div>
+              <p>Paste the NFT collection address.</p>
+              <div className={`flex border-2 border-base-300 bg-base-200 rounded-full text-accent`}>
+                <input
+                  className="input input-ghost focus-within:border-transparent focus:outline-none focus:bg-transparent focus:text-gray-400 h-[2.2rem] min-h-[2.2rem] px-4 border w-full font-medium placeholder:text-accent/50 text-gray-400"
+                  placeholder={"0x123"}
+                  name={"contractAddressInput"}
+                  autoComplete="off"
+                />
+              </div>
+            </div>
+
+            <button className="btn btn-primary" type="submit">
+              Search
+            </button>
+          </form>
         </div>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
